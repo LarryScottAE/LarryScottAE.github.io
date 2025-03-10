9931453336bd95f7dd4858314962f707f47cfaaf
Larry Scott <lscottae@gmail.com>
2025-03-10 13:18:44 -0700

    20250310

---
+++ script.js
@@ -0,0 +1,239 @@
+	
+	function winOpen(win)
+	{
+	 window.open(win,'_blank',"toolbar=no,scrollbars=yes,resizable=yes,top=100,left=100,width=1000,height=800"); 
+	}
+	
+	
+    function browserCheck() { 
+          let userAgent = navigator.userAgent;
+          let browser = "Unknown";
+          
+          // Detect Chrome
+          if (/Chrome/.test(userAgent) && !/Chromium/.test(userAgent)) {
+              browser = "Google Chrome";
+          }
+          // Detect Chromium-based Edge
+          else if (/Edg/.test(userAgent)) {
+              browser = "Microsoft Edge";
+          }
+          // Detect Firefox
+          else if (/Firefox/.test(userAgent)) {
+              browser = "Mozilla Firefox";
+          }
+          // Detect Safari
+          else if (/Safari/.test(userAgent)) {
+              browser = "Apple Safari";
+          }
+          // Detect Internet Explorer
+          else if (/Trident/.test(userAgent)) {
+              browser = "Internet Explorer";
+          }
+          
+      //	document.write(browser);
+          document.getElementById('browserID').innerHTML=browser;
+          return browser;
+      }
+	 
+	 
+  function routerReset() {
+   document.getElementById("routerDiv0").hidden = false;  // default
+   document.getElementById("routerDiv1").hidden = true;
+   document.getElementById("routerDiv2").hidden = true;
+   document.getElementById("routerDiv3").hidden = true;
+   document.getElementById("routerDiv4").hidden = true;
+   document.getElementById("routerDiv5").hidden = true;
+   document.getElementById("routerDiv6").hidden = true;
+   document.getElementById("routerDiv7").hidden = true;
+   document.getElementById("routerDiv8").hidden = true;
+   document.getElementById("routerDiv9").hidden = true;
+   document.getElementById("routerDiv10").hidden = true;
+  }
+ 
+ 
+  function router(screen) {
+  const button = document.getElementById("router" + screen);
+  let routerDiv = "routerDiv" + screen;
+//alert("routerDiv=" + screen.toString());
+//alert("routerDiv=" + routerDiv);
+
+  let buttonName = "";
+  if (screen == 1)  buttonName= "Router Face"; else 
+  if (screen == 2)  buttonName= "Router Rear"; else 
+  if (screen == 3)  buttonName= "PC Config 1"; else 
+  if (screen == 4)  buttonName= "PC Config 2"; else 
+  if (screen == 5)  buttonName= "PC Config 3"; else 
+  if (screen == 6)  buttonName= "Router Config 1"; else 
+  if (screen == 7)  buttonName= "Router Config 2"; else 
+  if (screen == 8)  buttonName= "Router Config 3"; else 
+  if (screen == 9)  buttonName= "Router Config 4"; else 
+  if (screen == 10)  buttonName= "Router Config 5"; 
+
+//alert("buttonName=" + buttonName);
+  let buttonHide = "Hide " + buttonName;
+  let buttonShow = "Show " + buttonName;
+
+  document.getElementById("routerDiv0").hidden = true;
+  if (button.textContent == buttonHide) 
+   {
+    button.textContent = buttonShow;
+	document.getElementById(routerDiv).hidden = true;
+   } 
+   else 
+   {
+    button.textContent = buttonHide;
+	document.getElementById(routerDiv).hidden = false;
+   }
+   
+   // if no divs visible, show default div 0
+   let foundVisibleDiv = false;
+   for (let i = 1; i <= 10; i++) {
+      let routerDivTest = "routerDiv" + i.toString();
+   // alert("routerDivTest=" + routerDivTest);
+
+      const div = document.getElementById(routerDivTest);
+      const isHidden = window.getComputedStyle(div).display === 'none';
+
+   // alert("routerDivTest=" + routerDivTest + " hidden=" + isHidden);
+
+      if (!(isHidden)) 
+	    {
+	    foundVisibleDiv=true;
+        break; 
+	    }
+  	}
+
+   if (foundVisibleDiv===false) 
+      routerReset();   
+ }
+  
+  
+function midiControlReset() {
+  document.getElementById("midiControlDiv0").hidden = false; // default
+  document.getElementById("midiControlDiv1").hidden = true;
+  document.getElementById("midiControlDiv2").hidden = true;
+  document.getElementById("midiControlDiv3").hidden = true;
+  document.getElementById("midiControlDiv4").hidden = true;
+  document.getElementById("midiControlDiv5").hidden = true;
+  document.getElementById("midiControlDiv6").hidden = true;
+ }
+ 
+ 
+function midiControl(screen) {
+ const button = document.getElementById("midiControl" + screen);
+ let midiControlDiv = "midiControlDiv" + screen;
+//alert("midiControlDiv=" + screen.toString());
+//alert("midiControlDiv=" + midiControlDiv);
+
+ let buttonName = "";
+ if (screen == 1)  buttonName= "Control 1"; else 
+ if (screen == 2)  buttonName= "Control 2"; else 
+ if (screen == 3)  buttonName= "Control 3"; else 
+ if (screen == 4)  buttonName= "Control 4"; else 
+ if (screen == 5)  buttonName= "Control 5"; else 
+ if (screen == 6)  buttonName= "Control 6"; 
+
+//alert("buttonName=" + buttonName);
+  let buttonHide = "Hide " + buttonName;
+  let buttonShow = "Show " + buttonName;
+
+  document.getElementById("midiControlDiv0").hidden = true;
+  if (button.textContent == buttonHide) 
+   {
+    button.textContent = buttonShow;
+	document.getElementById(midiControlDiv).hidden = true;
+  } 
+   else 
+   {
+    button.textContent = buttonHide;
+	document.getElementById(midiControlDiv).hidden = false;
+   }
+
+   // if no divs visible, show default div 0
+   let foundVisibleDiv = false;
+   for (let i = 1; i <= 6; i++) {
+      let midiControlDivTest = "midiControlDiv" + i.toString();
+   // alert("midiControlDivTest=" + midiControlDivTest);
+
+      const div = document.getElementById(midiControlDivTest);
+      const isHidden = window.getComputedStyle(div).display === 'none';
+
+   // alert("midiControlDivTest=" + midiControlDivTest + " hidden=" + isHidden);
+
+      if (!(isHidden)) 
+	    {
+	    foundVisibleDiv=true;
+        break; 
+	    }
+  	}
+
+   if (foundVisibleDiv===false) 
+      midiControlReset();   
+  } 
+   
+   
+   
+function midiOrganizeReset() {
+  document.getElementById("midiOrganizeDiv0").hidden = false;
+  document.getElementById("midiOrganizeDiv1").hidden = true;
+  document.getElementById("midiOrganizeDiv2").hidden = true;
+  document.getElementById("midiOrganizeDiv3").hidden = true;
+  document.getElementById("midiOrganizeDiv4").hidden = true;
+  document.getElementById("midiOrganizeDiv5").hidden = true;
+  document.getElementById("midiOrganizeDiv6").hidden = true;
+ }
+
+
+
+function midiOrganize(screen) {
+ const button = document.getElementById("midiOrganize" + screen);
+ let midiOrganizeDiv = "midiOrganizeDiv" + screen;
+//alert("midiOrganizeDiv=" + screen.toString());
+//alert("midiOrganizeDiv=" + midiOrganizeDiv);
+
+ let buttonName = "";
+ if (screen == 1)  buttonName= "Organize 1"; else 
+ if (screen == 2)  buttonName= "Organize 2"; else 
+ if (screen == 3)  buttonName= "Organize 3"; else 
+ if (screen == 4)  buttonName= "Organize 4"; else 
+ if (screen == 5)  buttonName= "Organize 5"; else 
+ if (screen == 6)  buttonName= "Organize 6"; 
+
+//alert("buttonName=" + buttonName);
+  let buttonHide = "Hide " + buttonName;
+  let buttonShow = "Show " + buttonName;
+
+  document.getElementById("midiOrganizeDiv0").hidden = true;
+  if (button.textContent == buttonHide) 
+   {
+    button.textContent = buttonShow;
+	document.getElementById(midiOrganizeDiv).hidden = true;
+  } 
+   else 
+   {
+    button.textContent = buttonHide;
+	document.getElementById(midiOrganizeDiv).hidden = false;
+   }  
+
+   // if no divs visible, show default div 0
+   let foundVisibleDiv = false;
+   for (let i = 1; i <= 6; i++) {
+      let midiOrganizeDivTest = "midiOrganizeDiv" + i.toString();
+   // alert("midiOrganizeDivTest=" + midiOrganizeDivTest);
+
+      const div = document.getElementById(midiOrganizeDivTest);
+      const isHidden = window.getComputedStyle(div).display === 'none';
+
+   // alert("midiOrganizeDivTest=" + midiOrganizeDivTest + " hidden=" + isHidden);
+
+      if (!(isHidden)) 
+	    {
+	    foundVisibleDiv=true;
+        break; 
+	    }
+  	}
+
+   if (foundVisibleDiv===false) 
+      midiOrganizeReset();   
+ }  
+   
